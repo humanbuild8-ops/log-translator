4e649d042f516e4aae5fd82f6e344e1c29a50d22
humanbuild8-ops <humanbuild.8@gmail.com>
2026-04-22 01:05:01 +0530

    Step 4: Added anomaly detection using Isolation Forest

--- parser/parser.py
+++ parser/parser.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import IsolationForest
 import pandas as pd
 
 def categorize_log(level):
@@ -31,5 +32,25 @@ for log in logs:
 
 # DataFrame
 df = pd.DataFrame(logs)
+# Convert category to numeric (ML needs numbers)
+df['category_num'] = df['category'].map({
+    'Critical': 3,
+    'Warning': 2,
+    'Info': 1
+})
+
+# Create model
+model = IsolationForest(contamination=0.2, random_state=42)
+
+# Train + predict
+df['anomaly'] = model.fit_predict(df[['category_num']])
+
+# Convert output (-1, 1) to readable form
+df['anomaly'] = df['anomaly'].map({
+    -1: 'Anomaly',
+    1: 'Normal'
+})
 print("\nDataFrame Output:\n")
 print(df)
+print("\nAnomaly Detection Output:\n")
+print(df[['level', 'category', 'anomaly', 'message']])
